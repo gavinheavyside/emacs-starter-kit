9813a532ace358d8995870ff27ce1dd6dd7655b6
Gavin Heavyside <gavin.heavyside@journeydynamics.com>
2010-05-20 15:11:23 +0100

    % bound to paren matching is not useful in Clojure

--- gavin/init.el
+++ gavin/init.el
@@ -93,13 +93,13 @@
 
 
 
-;; Vi-style parentheses matching
-(defun match-paren (arg)
-  "Go to the matching parenthesis if on parenthesis otherwise insert %."
-  (interactive "p")
-  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
-	((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-	(t (self-insert-command (or arg 1)))))
+;; ;; Vi-style parentheses matching
+;; (defun match-paren (arg)
+;;   "Go to the matching parenthesis if on parenthesis otherwise insert %n."
+;;   (interactive "p")
+;;   (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+;; 	((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+;; 	(t (self-insert-command (or arg 1)))))
 
 
 ;; Bind some keys
